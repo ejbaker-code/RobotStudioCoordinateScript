5164c2f45486f9ee1cfa123a17bcc9ca9d38fbd7
ejbaker-code <83137635+ejbaker-code@users.noreply.github.com>
2021-04-24 15:22:15 -0400

    Update to py script
    Labeled which parts are for which toolpaths, changed names of files to match names of files provided in repo

--- robotCoords.py
+++ robotCoords.py
@@ -1,5 +1,6 @@
 #takes tab delimited coordinates from robotcoords.txt, formats into RobotStudio code file robotcode.txt to move robot to specified coordinates
 
+#blue toolpath
 import csv
 import os
 coords = open ('bluerobotcoords.txt')
@@ -19,14 +20,6 @@ robotcode.write("CONST robtarget p10:=[[469.36,-103.94,412.05],[0.719835,-0.0230
 robotcode.write("PROC main() \n \t \t")
 robotcode.write(movep10)
 robotcode.write("\n\t\t\t" + varset + "\n\t\t" + movel)
-#coord_data  = open("robotcoords.txt", newline='\t')
-#v1 = coord_data.read()
-#print("v1 is : ", v1)
-#v2 = coord_data.read()
-#print("v2 is : ", v2)
-#v3 = coord_data.read()
-#print("v3 is : ", v3)
-#coord_data.close()
 for co in d:
     v1 = co[0]
     v2 = co[1]
@@ -37,13 +30,12 @@ robotcode.write("\n\t\t"+movep10)
 robotcode.write("\n\t ENDPROC \n ENDMODULE")
 robotcode.close()
 
-#takes tab delimited coordinates from robotcoords.txt, formats into RobotStudio code file robotcode.txt to move robot to specified coordinates
-
-coords = open ('spirocoords.txt')
+#gold toolpath
+coords = open ('goldcoords.txt')
 reader = csv.reader(coords, delimiter='\t')
 d = list(reader)
 coords.close()
-robotcode = open("spirocode.txt", "w")
+robotcode = open("goldrobotcode.txt", "w")
 tab = "\t"
 v1 = 0
 v2 = 0
